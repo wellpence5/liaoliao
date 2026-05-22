d362c4639e83c9ad964a531e20587b65a3c914b7
wellpence5 <alvinmuthomi22@gmail.com>
2026-05-22 14:49:16 +0800

    Added socket function

---
+++ liao.py
@@ -0,0 +1,16 @@
+import socket
+import threading
+
+def listen(host, port):
+    server = socket.socket()
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((host, port))
+    server.listen(1)
+    print(f"[*]Listening on {host}:{port}")
+    conn, addr = server.accept()
+    print(f"[*]Incoming connection from {addr[0]}:{addr[1]}")
+    server.close()
+    return conn
+
+conn = listen("0.0.0.0", 5000)
+print("Got  connection!", conn)
